38179bc0c3e3a0ae17573645ef968d13fcb0c206
shubham-mali7 <shubhammali31299@gmail.com>
2024-01-16 23:51:51 -0800

    template string

---
+++ file10.js
@@ -0,0 +1,11 @@
+// template string 
+
+let age = 24;
+let firstName = "shubham";
+let prof = "developer";
+
+// let string1 = "My name is " + firstName + "I am " + age + "years and I am a " + prof ;
+// console.log(string1);// very tedious work
+
+let string = `I am ${firstName} my age is ${age}, I am a ${prof}`;
+console.log(string); // template string 
--- file9.js
+++ file9.js
@@ -1,1 +1,17 @@
 // string concatination 
+
+let string1 = "10";
+let string2 = "12";
+
+// let newString = string1 + string2;
+// console.log(newString);
+// console.log(typeof newString); // o/p - string
+
+// let newString = +string1 + +string2;
+// console.log(newString);
+// console.log(typeof newString); // o/p - number 
+
+// another way-----------
+let newString = Number(string1) + Number(string2);
+console.log(newString);
+console.log(typeof newString); // o/p - number 
