bbbc26d5ced4bfc0d1104cfa2deedb55f4d5ae88
shubham-mali7 <shubhammali31299@gmail.com>
2024-01-16 18:36:35 -0800

    second commit -(completed string methods)

---
+++ file7.js
@@ -0,0 +1,24 @@
+//String Methods 
+// toUpperCase()
+// toLowerCase()
+// slice 
+
+let firstName = "shubham"
+// let firstName = "   shubham   "
+// let length = firstName.length
+// console.log(length)
+
+// firstName = firstName.trim();
+// console.log(firstName);
+// console.log(firstName.length)
+
+// const capitalName = firstName.toUpperCase();
+// console.log(capitalName); // o/p : SHUBHAM 
+
+// const newName = firstName.toLocaleLowerCase();
+// console.log(newName); //shubham
+
+
+// let newName = firstName.slice(0,5); // o/p shubh
+let newName = firstName.slice(1); // o/p hubham
+console.log(newName);
---
+++ file8.js
@@ -0,0 +1,36 @@
+// type of operator - typeof()
+
+//data types (primitive data types)
+// string "shubham"
+// numbers 2, 4, 5.6 
+// Boolean
+// undefined
+// null 
+// BigInt 
+// symbol 
+
+let age = 22;
+let firstName = "shubham";
+// console.log(typeof(age)); // o/p number 
+
+// convert no to string 
+// 22-->"22"
+
+// age = age + ""
+// console.log(typeof age); // o/p string
+
+//Another way ----------
+// age = String(age);
+// console.log(typeof age); // o/p string 
+
+// convert string to number
+// "22" --> 22
+
+//Another way ------
+// let myStr = "nonsense";
+// myStr = Number(myStr);
+// console.log(typeof myStr); // o/p number 
+
+// let myStr = "32";
+// myStr = +myStr;
+// console.log(typeof myStr) // o/p number 
